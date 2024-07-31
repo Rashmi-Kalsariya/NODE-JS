5decf1f142adeda320c278f2e1af273ffcb15b95
Mrugen-Kalsariya <mrugenk30@gmail.com>
2024-07-31 11:25:32 +0530

    NODE

---
+++ Node_Api/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("WelCome To The First Node API!!");
+});
+
+app.get("/api", (req, res) => {
+    res.send("Welcome To API!!")
+});
+
+
+//Port
+
+app.listen(8090, () => {
+    console.log("Starting Node...");
+})
