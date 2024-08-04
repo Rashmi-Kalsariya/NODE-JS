f44d535df20d7184c9f57819bb1b85cc8dad4a10
Rashmi-Kalsariya <rashmikalsariya02@gmail.com>
2024-08-05 03:28:10 +0530

    NODE

--- Data_Base/index.js
+++ Data_Base/index.js
@@ -3,11 +3,14 @@
 const dbconnect = require("./db");
 const express = require("express");
 
+
 const app = express();
 
 app.use(express.json())
 
 const User = require("./User.schema");
+const isValid = require("./middleware/middleware");
+
 
 app.get("/", async (res, req) => {
     let data = await User.find();
@@ -16,10 +19,25 @@ app.get("/", async (res, req) => {
 
 // Post For Creates Data
 
-app.post("/", async (res, req) => {
+app.post("/", isValid, async (res, req) => {
     let data = await User.create(req.body);
-    res.send(data);
+    res.send(User);
 })
+
+
+app.patch("/:id", async (req, res) => {
+    let { id } = req.params;
+    let data = await User.findByIdAndUpdate(id, req.body);
+    res.send(data);
+});
+
+app.delete("/:id", async (req, res) => {
+    let { id } = req.params;
+    let data = await User.findByIdAndDelete(id);
+    res.send(data);
+});
+
+
 
 
 app.listen(8090, () => {
